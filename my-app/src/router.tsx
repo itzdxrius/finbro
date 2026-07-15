8cf1b198b1bd918fb8b7d4b552c6bcf326b4fd0f
Coolman1234 Aidan <markedaidan1010@gmail.com>
2026-07-15 17:05:37 -0400

    Added all main files to start working

---
+++ my-app/src/router.tsx
@@ -0,0 +1,16 @@
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Budget from "./pages/Budget"
+import Dashboard from "./pages/Dashboard"
+import Setting from "./pages/Setting"
+import History from "./pages/History"
+import {createBrowserRouter} from "react-router-dom";
+
+export const router = createBrowserRouter([
+    {path:"/login", element:<Login/>},
+    {path:"/dashboard", element:<Dashboard/>},
+    {path:"/register", element:<Register/>},
+    {path:"/budget", element:<Budget/>},
+    {path:"/setting", element:<Setting/>},
+    {path:"/history", element:<History/>}
+]);
