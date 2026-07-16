e243ce46632d54d514ef32645df6485758931412
Coolman1234 Aidan <markedaidan1010@gmail.com>
2026-07-16 13:25:55 -0400

    added sign in func

---
+++ my-app/src/lib/auth.ts
@@ -0,0 +1,9 @@
+import {supabase} from "./subabase";
+
+
+//For sign in
+export async function signIn(email:string,password:string){
+    const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+    if (error) throw error;
+    return data;
+}
