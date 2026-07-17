337580712b0eb395bf68a567dff6cd99b4d80029
Dai <dariusg@andrew.cmu.edu>
2026-07-17 14:59:54 -0400

    refactored plaidapi.ts and moved testing logic to test.ts

---
+++ my-app/server/test.ts
@@ -0,0 +1,14 @@
+import {initialize_plaid_account} from "./plaidapi"
+import {sync_transactions} from "./transactions"
+import * as fs from "fs";
+
+
+async function test(user, pass) {
+    const token = await initialize_plaid_account(user, pass);
+    const transactions = await sync_transactions(token) 
+    fs.writeFileSync("transactions.json", JSON.stringify(transactions, null, 2))
+}
+
+let user = "user_good";
+let pass = "pass_good";
+test(user, pass) 
--- my-app/server/transactions.ts
+++ my-app/server/transactions.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
-import * as fs from "fs";
 import "dotenv/config";
 
 const TRANSACTIONS_URL = "https://sandbox.plaid.com/transactions/sync";
-async function sync_transactions(access_token) {
+export async function sync_transactions(access_token) {
         
     const transactions_post_data = {
         client_id: process.env.PLAID_CLIENT_ID,
@@ -13,17 +12,5 @@ async function sync_transactions(access_token) {
     const transactions = await axios.post(TRANSACTIONS_URL, transactions_post_data)
     return transactions.data;
 }
-
-
-async function sync_transactions_test(access_token) {
-    const transactions = await sync_transactions(access_token)
-    // replace below with a write to supabase
-    fs.writeFileSync("transactions.json", JSON.stringify(transactions, null, 2))
-}
-
-// const access_token = *get from supabase
-const access_token = process.env.PLAID_ACCESS_TOKEN;
-sync_transactions_test(access_token)
-
 
 
