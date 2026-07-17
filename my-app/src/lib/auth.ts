d141fe59c0c3b7d4c3924943668f9fd4cf22fdb4
Coolman1234 Aidan <markedaidan1010@gmail.com>
2026-07-17 12:35:57 -0400

    Added all necessary auth funcs such as login, register, signin, get session, and get user

--- my-app/src/lib/auth.ts
+++ my-app/src/lib/auth.ts
@@ -1,4 +1,5 @@
 import {supabase} from "./supabase";
+import type { Session } from "@supabase/supabase-js";
 
 
 //For sign in
@@ -8,7 +9,45 @@ export async function signIn(email:string,password:string){
     return data;
 }
 
+//For registering
 export async function signUp(email:string, password: string){
-    
+    const {data,error} = await supabase.auth.signUp({
+        email:email,
+        password: password
+    })
+    if (error) throw error;
+    return data;
 }
+
+//logout
+export async function signOut() {
+    const { error } = await supabase.auth.signOut();
+    if (error) throw error;
+}
+
+//Gets the user
+export async function getCurrentUser(){
+    //This grabs user inside data
+    const {data:{user}, error} = await supabase.auth.getUser();
+    if (error) throw error;
+    return user
+}
+
+//gets session
+export async function getSession(){
+    const {data:{session},error} = await supabase.auth.getSession();
+    if(error) throw error
+    return session
+}
+
+//We use this function to constantly check the session making sure that the user is allowed
+//For example if user opens another tab and logs out of that tab then the original tab will also log out
+/*
+export function onAuthchange(callback:(session: Session|null)=>void){
+    const {data:{subscription}} = supabase.auth.onAuthStateChange(
+        (_event,session)=>callback(session)
+    )
+    return ()=>subscription.unsubscribe();
+}
+*/ 
 
