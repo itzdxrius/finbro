78755ff7ce9eaadfbe30e5cb0db427ee64617293
Dai <dariusg@andrew.cmu.edu>
2026-07-17 14:34:28 -0400

    refactored transactions.ts to accept external access token

--- my-app/server/transactions.ts
+++ my-app/server/transactions.ts
@@ -3,15 +3,27 @@ import * as fs from "fs";
 import "dotenv/config";
 
 const TRANSACTIONS_URL = "https://sandbox.plaid.com/transactions/sync";
-async function sync_transactions() {
-    const access_token = process.env.PLAID_ACCESS_TOKEN;    
+async function sync_transactions(access_token) {
+        
     const transactions_post_data = {
         client_id: process.env.PLAID_CLIENT_ID,
         secret: process.env.PLAID_CLIENT_SECRET,
         access_token: access_token
     }
     const transactions = await axios.post(TRANSACTIONS_URL, transactions_post_data)
-    fs.writeFileSync("transactions.json", JSON.stringify(transactions.data, null, 2))
+    return transactions.data;
 }
 
-sync_transactions()
+
+async function sync_transactions_test(access_token) {
+    const transactions = await sync_transactions(access_token)
+    // replace below with a write to supabase
+    fs.writeFileSync("transactions.json", JSON.stringify(transactions, null, 2))
+}
+
+// const access_token = *get from supabase
+const access_token = process.env.PLAID_ACCESS_TOKEN;
+sync_transactions_test(access_token)
+
+
+
